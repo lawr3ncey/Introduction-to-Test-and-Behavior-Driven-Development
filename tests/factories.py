7f19606657eeae9664463d84d3e0e1b3a15beedc
lawr3ncey <babelonia.l.bscs@gmail.com>
2026-02-09 09:32:29 -0500

    Fix lint indentation in factories

--- tests/factories.py
+++ tests/factories.py
@@ -31,7 +31,7 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+# Add code to create Fake Products
     name = factory.Faker("word")
     description = factory.Faker("text")
     price = FuzzyDecimal(0.5, 2000.0, 2)
